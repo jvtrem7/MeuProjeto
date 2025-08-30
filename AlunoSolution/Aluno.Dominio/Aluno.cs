6f93e90be29cea6fd039dbf7a9e6f2d678364ffc
joao vcitor <jvsampaiio15@gmail.com>
2025-08-29 21:20:39 -0300

    criação das classes

---
+++ AlunoSolution/Aluno.Dominio/Aluno.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Aluno.Dominio
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+
+        public bool ehmaiordeidade()
+        {
+            return Idade >= 18;
+        }
+    }
+}
---
+++ AlunoSolution/Aluno.Repositorio/IAlunoRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aluno.Dominio;
+
+namespace Aluno.Repositorio
+{
+    public interface IAlunoRepositorio
+    {
+        void Adicionar();
+        List<int> Listar();
+    }
+}
